93cc62043b2a57e8bf37b8679b05fada5e579d44
SergeiKorovin <sergei.korovin1985@mail.ru>
2024-08-23 14:07:33 +0500

    1,2,3,4 tasks

---
+++ modul_1_2/main.py
@@ -0,0 +1,8 @@
+count_completed_homework = 12
+count_hours = 1.5
+course_name = 'Python'
+time = count_hours / count_completed_homework
+print(f'Курс: {course_name}, всего задач: {count_completed_homework}, '
+      f'затрачено часов: {count_hours}, среднее время выполнения {time} часа.')
+print('Курс:', course_name, ', всего задач:', count_completed_homework,  ', затрачено часов:', count_hours, ', среднее время выполнения', time, 'часа.')
+print('Курс: ' + course_name + ', всего задач: ' + str(count_completed_homework) + ', затрачено часов: ' + str(count_hours) + ', среднее время выполнения ' + str(time) + ' часа.')
---
+++ modul_1_3/main.py
@@ -0,0 +1,9 @@
+name ='Sergey'
+print('Name:', name)
+age = 38
+print('Age:', age)
+age = age + 1
+print('New age:', age)
+is_student = True
+print('Is student:', is_student)
+
